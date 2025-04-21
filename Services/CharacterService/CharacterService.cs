e7deed20577a1c704efc4e263f88601392c173b3
Othyem86 <emil.foeger@gmail.com>
2025-04-21 19:34:36 +0200

    CHANGE: Delete Character

--- Services/CharacterService/CharacterService.cs
+++ Services/CharacterService/CharacterService.cs
@@ -28,6 +28,31 @@ public class CharacterService : ICharacterService
         };
     }
 
+    public async Task<ServiceResponse<List<GetCharacterDto>>> DeleteCharacter(int id)
+    {
+        var response = new ServiceResponse<List<GetCharacterDto>>();
+
+        try
+        {
+            var character = characters.FirstOrDefault(c => c.Id == id);
+            
+            if (character is null)
+                throw new Exception($"Character with Id '{id}' not found.");
+
+            characters.Remove(character);
+
+            response.Data = characters.Select(c => _mapper.Map<GetCharacterDto>(c)).ToList();
+
+        }
+        catch (Exception ex)
+        {
+            response.Success = false;
+            response.Message = ex.Message;
+        }
+
+        return response;
+    }
+
     public async Task<ServiceResponse<List<GetCharacterDto>>> GetAllCharacters()
     {
         return new ServiceResponse<List<GetCharacterDto>>
